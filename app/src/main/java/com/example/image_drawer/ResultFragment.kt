e05c0b4507923d6901dc044854fbc98410bb1568
aperevala <92289919+aperevala@users.noreply.github.com>
2022-04-03 03:09:00 +0300

    seekbar

--- app/src/main/java/com/example/image_drawer/ResultFragment.kt
+++ app/src/main/java/com/example/image_drawer/ResultFragment.kt
@@ -1,22 +1,27 @@
 package com.example.image_drawer
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import androidx.viewpager2.widget.ViewPager2
+
 
 class ResultFragment : Fragment() {
 
     private lateinit var viewModel: ResultFragmentVM
+    lateinit var viewPager: ViewPager2
+    lateinit var seekBar : SeekBar
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        lateinit var viewPager: ViewPager2
+
         val args by navArgs<ResultFragmentArgs>()
 
         viewModel = ResultFragmentVM(args.id)
@@ -24,8 +29,22 @@ class ResultFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_result, container, false)
 
         viewPager = view.findViewById(R.id.imagesOriginal)
-
+        seekBar = view.findViewById(R.id.seek_bar)
         viewPager.adapter = viewModel.adapter
+
+        seekBar.max = 10
+
+        seekBar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                viewPager.currentItem = progress
+            }
+        })
+
+
 
         viewModel.loadLesson()
 
